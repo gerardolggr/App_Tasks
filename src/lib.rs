d6d8ca0929abccd78bf9887d54d4bcda76c462ff
Luis Gerardo Gonzalez Ramirez <104467605+gerardolggr@users.noreply.github.com>
2026-03-01 18:30:40 -0600

    Refactor TasksAccount and NewTasksAccount structures
    Updated TasksAccount and NewTasksAccount structures to include new fields and annotations for better account management.

--- src/lib.rs
+++ src/lib.rs
@@ -12,21 +12,49 @@ pub mod Tareas{
 }
 
 //Cuenta principal por Usuario que almacena sus tareas
+#[account]
+#[derive(InitSpace)]
 pub struct TasksAccount{
-    owner: Pubkey, 
+    owner: Pubkey,
+
+    #[max_len(60)] 
     nombre_tablero: String, 
+
+    #[max_len(10)]
     tareas: Vec<Task>, 
 }
 
 //Estructura que almacena las tareas
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, InitSpace, PartialEq, Debug)]
 pub struct Task{
+    #[max_len(60)]
     titulo: String,
+
     completada: bool, 
 }
 
-//Define las cuentas necesarias para la instrucción
+//Define lo que necesita una cuenta para ejecutar la instrucción
+#[derive(Accounts)]
 pub struct NewTasksAccount{
-    pub owner: Signer,
+    #[account(mut)]
+    pub owner: Signer<'info>, 
+
+    #[account(
+        init, payer = owner, 
+        space = TasksAccount::INIT_SPACE + 8, 
+        seeds = [b"Tareas", 
+        owner.key().as_ref(), 
+        bump])]
     pub task_account: Account<'info, TasksAccount>,
+
+
     pub system_program: Program<'info, System>, 
+}
+
+//Estructura que ayuda a dar las instrucciones (CRUD) de la cuenta ya creada
+pub struct UpdateTask{
+    pub owner: Signer<'info>,
+
+    #[account(mut)]
+    pub task_account: Account<'info, TasksAccount>, 
 }
